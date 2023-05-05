da6280a8f8be1010eae64264c69a90258a288173
malerinc <malerinc>
2023-05-05 02:42:04 +0200

    Updated controls package install script to include readme.

--- 2_control/slapstack-controls/setup.py
+++ 2_control/slapstack-controls/setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
 setup(name='slapstack-controls',
-      version='0.0.1',
-      install_requires=['slapstack', 'numpy'],
+      version='0.1.1',
+      python_requires='>3.6.8',
+      install_requires=[
+            'slapstack', 'numpy'
+      ],
+      description="Storage Strategies for the SLAPStack simulation Framework.",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       packages=find_packages(),
       )
